2db753ea267513db81a101c531727e0abcab7a36
Adipp <emangadayanglain@gmail.com>
2023-10-19 07:18:23 +0700

    [A] style onBoard

--- lib/pages/onboarding/onboard.dart
+++ lib/pages/onboarding/onboard.dart
@@ -1,33 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 
-class OnBoarding extends StatelessWidget {
+class OnBoarding extends StatefulWidget {
   const OnBoarding({super.key});
 
+  @override
+  State<OnBoarding> createState() => _OnBoardingState();
+}
+
+bool showBottomPart = true;
+
+class _OnBoardingState extends State<OnBoarding> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: IntroductionScreen(
+        showBottomPart: showBottomPart,
+        showDoneButton: true,
+        showBackButton: false,
+        showNextButton: false,
         pages: [
           PageViewModel(
-            image: Image.asset("assets/image/reading.png"),
-            title: 'Kesusahan dalam belajar ?',
+            image: Image.asset("assets/images/reading.png"),
+            title: 'Kesusahan dalam belajar?',
             body: '[nama aplikasi] memberikan kemudahan dalam belajar karena kamu bisa belajar dimanapun dan kapanpun ',
           ),
           PageViewModel(
-            image: Image.asset("assets/image/laying.png"),
+            image: Image.asset("assets/images/laying.png"),
             title: 'Materi yang beragam',
             body: '[nama aplikasi] mempunyai beragam materi menarik yang bisa dipelajari dari sekarang ',
           ),
           PageViewModel(
-            image: Image.asset("assets/image/chilling.png"),
+            image: Image.asset("assets/images/chilling.png"),
             title: 'Harga terjangkau',
             body: 'Kamu bisa menikmati fitur di [nama aplikasi] tanpa harus menyiksa dompetmu ',
           ),
         ],
-        back: Icon(Icons.arrow_back_ios_rounded),
-        next: Icon(Icons.navigate_next),
-        done: ElevatedButton(onPressed: () {}, child: Text("Get Started")),
+        // back: Icon(Icons.arrow_back_ios_rounded),
+        // next: Icon(Icons.navigate_next),
+        done: ElevatedButton(
+            onPressed: () {
+              setState(() {
+                showBottomPart =
+                    false; // Sembunyikan bagian bawah setelah "Get Started" ditekan
+              });
+            },
+            child: Text("Get Started")),
         onDone: () {},
       ),
       // bottomNavigationBar: Container(
