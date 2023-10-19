2f4f53105bbbda1fe0f4f0e70c86a4aa26f34d27
Adipp <emangadayanglain@gmail.com>
2023-10-19 21:16:52 +0700

    [A} Fixing style Image logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ilearn_project/pages/onboarding/onboard.dart';
 import 'package:ilearn_project/pages/register_page/register.dart';
 
 void main() {
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Regsiter(),
+      home: Register(),
     );
   }
 }
--- lib/pages/register_page/register.dart
+++ lib/pages/register_page/register.dart
@@ -5,69 +5,76 @@ import 'package:ilearn_project/core/themes.dart';
 import 'package:ilearn_project/pages/register_page/widget/checkbox.dart';
 import 'package:ilearn_project/pages/register_page/widget/userinput.dart';
 
-class Regsiter extends StatelessWidget {
-  const Regsiter({super.key});
+class Register extends StatelessWidget {
+  const Register({super.key});
+
+
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          children: [
-            SizedBox(height: 20),
-            Align(
-              alignment: Alignment.center,
-              child: Text(
-                  "Register",
-                  style: TextStyle(
-                      fontSize: 23,
-                      fontWeight: FontWeight.w500
-                  )
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: screenHeight * 0.1),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                    "Register",
+                    style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.w500
+                    )
+                ),
               ),
-            ),
-            SizedBox(height: 40),
-            InputUser(label: 'First Name', icon: Icons.person, textInputType: TextInputType.name,),
-            InputUser(label: 'Last Name', icon: Icons.person, textInputType: TextInputType.name,),
-            InputUser(label: 'Email', icon: Icons.mail, textInputType: TextInputType.emailAddress,),
-            InputUser(label: 'Password', icon: Icons.lock, textInputType: TextInputType.visiblePassword,),
-            CheckboxWidget(),
-            SizedBox(height: 15),
-            ButtonInputUser(onPressed: (){}),
-            SizedBox(height: 20,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 2,
-                  width: 150,  // Lebar garis
-                  color: peachColor,
-                ),
-                SizedBox(width: 10,),
-                Text('or', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500)),
-                SizedBox(width: 10,),
-                Container(
-                  height: 2,
-                  width: 150,  // Lebar garis
-                  color: peachColor,
-                ),
-              ],
-            ),
-            SizedBox(height: 20,),
-            Logo(),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Already have an account?"),
-                SizedBox(width: 10),
-                Text("Login", style: TextStyle(
-                  color: Color(0xFFFFD1AD)
-                ),)
-              ],
-            )
-            // Logo()
-          ],
+              SizedBox(height: 40),
+              InputUser(label: 'First Name', icon: Icons.person, textInputType: TextInputType.name,),
+              InputUser(label: 'Last Name', icon: Icons.person, textInputType: TextInputType.name,),
+              InputUser(label: 'Email', icon: Icons.mail, textInputType: TextInputType.emailAddress,),
+              InputUser(label: 'Password', icon: Icons.lock, textInputType: TextInputType.visiblePassword,),
+              CheckboxWidget(),
+              SizedBox(height: 15),
+              ButtonInputUser(onPressed: (){}),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 2,
+                    width: 150,  // Lebar garis
+                    color: peachColor,
+                  ),
+                  SizedBox(width: 10,),
+                  Text('or', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500)),
+                  SizedBox(width: 10,),
+                  Container(
+                    height: 2,
+                    width: 150,  // Lebar garis
+                    color: peachColor,
+                  ),
+                ],
+              ),
+              SizedBox(height: 20,),
+              Logo(),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Already have an account?"),
+                  SizedBox(width: 10),
+                  Text("Login", style: TextStyle(
+                    color: Color(0xFFFFD1AD)
+                  ),)
+                ],
+              )
+              // Logo()
+            ],
+          ),
         ),
       ),
     );
