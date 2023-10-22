9d6f3ff79a4b1c51aafab7eaa9bc809d22b620f4
Alyaa Rana Raya <alyaaranaraya@gmail.com>
2023-10-22 16:02:18 +0700

    [Alyaa] add homeee

--- lib/pages/home_page/widget/appbar.dart
+++ lib/pages/home_page/widget/appbar.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
 
-class AppBar extends StatelessWidget {
-  const AppBar({super.key});
+import '../../../core/themes.dart';
+
+class AppBarCustom extends StatelessWidget {
+  const AppBarCustom({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    return Container(
+      width: screenWidth,
+      height: screenHeight * 0.1, // Sesuaikan dengan tinggi yang diinginkan
+      padding: EdgeInsets.only(top: screenHeight * 0.035),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Text("Start Learning", style: titlePage()),
+          ),
+          Stack(
+            alignment: Alignment.bottomRight,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage("assets/images/avatar.png")
+                as ImageProvider<Object>,
+              ),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Container(
+                  width: screenWidth * 0.035,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.white, width: 2),
+                  ),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.red,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
