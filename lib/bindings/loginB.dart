7dbea539dabfab908a3f748b1b051fd6afb71c3e
Adipp <emangadayanglain@gmail.com>
2023-10-26 15:15:04 +0700

    [Adip] add splash screen

--- lib/bindings/loginB.dart
+++ lib/bindings/loginB.dart
@@ -1,9 +1,12 @@
 import 'package:get/get.dart';
-import 'package:ilearn_project/controllers/loginPageController.dart';
+import 'package:ilearn_project/controllers/login_controller/loginPageController.dart';
 
-class LoginB implements Bindings {
+class LoginB extends Bindings{
   @override
   void dependencies() {
     Get.put(LoginPageController());
+    // TODO: implement dependencies
   }
+
+  
 }
