1d241b58a0bca33953b9a51d0cf7ddb00aaa680a
Adipp <emangadayanglain@gmail.com>
2023-11-05 13:52:56 +0700

    [Adip] commit changes

--- lib/pages/home_page/widget/courses_card.dart
+++ lib/pages/home_page/widget/courses_card.dart
@@ -10,42 +10,43 @@ class ForYou extends StatelessWidget {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
 
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: [
-          for (int i = 1; i < 8; i++)
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-              padding: EdgeInsets.symmetric(
-                vertical: screenHeight * 0.02, // Adjust as needed
-                horizontal: screenWidth * 0.05, // Adjust as needed
-              ),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(screenHeight * 0.025), // Adjust as needed
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: screenHeight * 0.008, // Adjust as needed
-                    offset: Offset(0, screenHeight * 0.004), // Adjust as needed
-                  ),
-                ],
-              ),
-              child: SingleChildScrollView( // Wrap content with SingleChildScrollView
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("HTML & CSS", style: titleCard()),
-                    Text("12 Exercises", style: subTitleCard()),
-                    Image.asset("assets/images/htmlcard.png", width: screenWidth * 0.19, height: screenHeight * 0.1),
-                  ],
-                ),
-              ),
+    return Container(
+      height: screenHeight * 0.25,
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: 7,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (BuildContext context, int i) {
+          return Container(
+            margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+            padding: EdgeInsets.symmetric(
+              vertical: screenHeight * 0.02, // Adjust as needed
+              horizontal: screenWidth * 0.05, // Adjust as needed
             ),
-        ],
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(
+                  screenHeight * 0.025), // Adjust as needed
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: screenHeight * 0.008, // Adjust as needed
+                  offset: Offset(0, screenHeight * 0.004), // Adjust as needed
+                ),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("HTML & CSS", style: titleCard()),
+                Text("12 Exercises", style: subTitleCard()),
+                Image.asset("assets/images/htmlcard.png",
+                    width: screenWidth * 0.19, height: screenHeight * 0.1),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
 }
-
