cefa85522597be44f52ba48bb12bd18fb6d13bca
Adipp <emangadayanglain@gmail.com>
2023-12-09 15:08:49 +0700

    [Adip] add Loading feature

--- lib/pages/loading_page/widget/ondoneloading.dart
+++ lib/pages/loading_page/widget/ondoneloading.dart
@@ -3,19 +3,11 @@ import 'package:get/get.dart';
 import 'package:ilearn_project/core/themes.dart';
 import 'package:lottie/lottie.dart';
 
-class OnDoneLoading extends StatefulWidget {
+class OnDoneLoading extends StatelessWidget {
   final String gif;
   final String text;
-
   const OnDoneLoading({Key? key, required this.gif, required this.text})
       : super(key: key);
-
-  @override
-  _OnDoneLoadingState createState() => _OnDoneLoadingState();
-}
-
-class _OnDoneLoadingState extends State<OnDoneLoading> {
-  bool animationEnded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -26,27 +18,17 @@ class _OnDoneLoadingState extends State<OnDoneLoading> {
         Stack(
           alignment: Alignment.center,
           children: <Widget>[
-            Lottie.asset(widget.gif,
-                repeat: false,
-                animate: !animationEnded,
-                onAnimationEnd: _onAnimationEnd),
-            if (animationEnded)
-              Positioned(
-                top: Get.height * 0.36,
-                child: Text(
-                  widget.text,
-                  style: titleCard(size: 30, color: primaryColor),
-                ),
+            Container(child: Lottie.asset(gif, repeat: false, animate: false)),
+            Positioned(
+              top: Get.height * 0.36,
+              child: Text(
+                text,
+                style: titleCard(size: 30, color: primaryColor),
               ),
+            )
           ],
         )
       ],
     );
-  }
-
-  void _onAnimationEnd() {
-    setState(() {
-      animationEnded = true;
-    });
   }
 }
